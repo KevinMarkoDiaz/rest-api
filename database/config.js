c4e30a03031d20f241407e22ed70e24a6e71d9ac
Kevin Diaz <kevindi0695@gmail.com>
2021-07-22 10:08:40 -0300

    segundo commit

--- database/config.js
+++ database/config.js
@@ -14,7 +14,7 @@ const dbConnection = async()=>{
 
     } catch (error) {
         
-        console,log(error);
+        console.log(error);
         throw new Error('Error al inicializar BD');
     }
 
